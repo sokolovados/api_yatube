716b8cf56fc341d9613e119d49b80da52c9b92ba
Вадим Соколов <sokolovadosolo@gmail.com>
2022-07-05 12:55:31 +0300

    Flake 8 test fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions
 
-from permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 
